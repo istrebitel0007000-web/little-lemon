5a3728ce9ca72e9cb76c4a720c6b470325870dde
Zafarshoh <istrebitel0007000@gmail.com>
2026-04-30 10:05:36 +0500

    cleanup: remove test2, myproject, and root-level shared apps; keep only littlelemon

--- addd/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from django.urls import include, path
-path('app/', include('addd.urls'))
-# Create your tests here.
-        
--- firstapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def function_view(request):
-    return HttpResponse("Hello from function!")
-
-# Create your views here.
